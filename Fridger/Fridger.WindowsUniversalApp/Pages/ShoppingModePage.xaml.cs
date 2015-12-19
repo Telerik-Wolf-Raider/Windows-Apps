8400c0174a5eb0c55e4f6ebb01cadbeb0f5c69be
pepinho24 <p_mil4ev@hotmail.com>
2015-12-19 18:58:07 +0200

    Added getting current location of the user in the shopping mode page (to save the current location as a good store)

--- Fridger/Fridger.WindowsUniversalApp/Pages/ShoppingModePage.xaml.cs
+++ Fridger/Fridger.WindowsUniversalApp/Pages/ShoppingModePage.xaml.cs
@@ -5,8 +5,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -23,20 +26,23 @@ namespace Fridger.WindowsUniversalApp.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ShoppingModePage 
+    public sealed partial class ShoppingModePage
     {
         private string pageTitle;
+        private Geolocator geolocator;
         public ShoppingModePage()
         {
             this.InitializeComponent();
+            this.geolocator = new Geolocator();
+
             var contentViewModel = new ProductsContentViewModel();
             contentViewModel.Products = new List<ProductViewModel>()
-      {
-      new ProductViewModel { ImgSource = "https://i.ytimg.com/vi/UIrEM_9qvZU/maxresdefault.jpg", ProductName="Product name 1" },
-                new ProductViewModel { ImgSource = "https://i.ytimg.com/vi/UIrEM_9qvZU/maxresdefault.jpg", ProductName="Product name 2" },
-                new ProductViewModel { ImgSource = "https://i.ytimg.com/vi/UIrEM_9qvZU/maxresdefault.jpg", ProductName="Product name 3" },
-                new ProductViewModel { ImgSource = "https://i.ytimg.com/vi/UIrEM_9qvZU/maxresdefault.jpg", ProductName="Product name 4" },
-      };
+                      {
+                      new ProductViewModel { ImgSource = "https://i.ytimg.com/vi/UIrEM_9qvZU/maxresdefault.jpg", ProductName="Product name 1" },
+                                new ProductViewModel { ImgSource = "https://i.ytimg.com/vi/UIrEM_9qvZU/maxresdefault.jpg", ProductName="Product name 2" },
+                                new ProductViewModel { ImgSource = "https://i.ytimg.com/vi/UIrEM_9qvZU/maxresdefault.jpg", ProductName="Product name 3" },
+                                new ProductViewModel { ImgSource = "https://i.ytimg.com/vi/UIrEM_9qvZU/maxresdefault.jpg", ProductName="Product name 4" },
+                      };
             this.ContentViewModel = contentViewModel;
             this.ViewModel = new ShoppingPageViewModel(contentViewModel);
         }
@@ -52,7 +58,7 @@ namespace Fridger.WindowsUniversalApp.Pages
                 this.DataContext = value;
             }
         }
-        
+
         public IContentViewModel ContentViewModel { get; set; }
 
         public IEnumerable<ProductViewModel> Products { get; set; }
@@ -75,6 +81,33 @@ namespace Fridger.WindowsUniversalApp.Pages
             var dialog = new MessageDialog(message);
             dialog.Commands.Add(new UICommand("OK"));
             await dialog.ShowAsync();
+        }
+
+        private async void OnSaveCurrentLocationClick(object sender, RoutedEventArgs e)
+        {
+            // Request permission to access location
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            if (accessStatus == GeolocationAccessStatus.Allowed)
+            {
+                // Make the request for the current position
+                Geoposition pos = await this.geolocator.GetGeopositionAsync().AsTask();
+                var lat = pos.Coordinate.Latitude;
+                var lon = pos.Coordinate.Longitude;
+
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                 {
+                     var message = string.Format("Your current location is:  Latitude:{0}, Longitude: {1}", lat, lon);
+                     var dialog = new MessageDialog(message);
+                     dialog.Commands.Add(new UICommand("OK"));
+                     await dialog.ShowAsync();
+                 });
+            }
+            else
+            {
+                var dialog = new MessageDialog("You denied geolocation!");
+                dialog.Commands.Add(new UICommand("OK"));
+                await dialog.ShowAsync();
+            }
         }
     }
 }
--- Windows-Applications-master/Windows-Applications-master/WindowsUniversalPlatform_Demos/ViewsDemo/ViewModels/CatViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ViewsDemo.ViewModels
-{
-  public class CatViewModel
-  {
-    public string Name { get; set; }
-    public string ImgSrc { get; set; }
-    public string Value { get; internal set; }
-  }
-}
