18b0bdd4d27eee3493485b42cfc90b6a60785e19
pepinho24 <p_mil4ev@hotmail.com>
2015-12-19 15:56:25 +0200

    Added bottom app bar navigation using AppShell page(some kind of master page or page wrapper)

--- Fridger/Fridger.WindowsUniversalApp/App.xaml.cs
+++ Fridger/Fridger.WindowsUniversalApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fridger.WindowsUniversalApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,20 +18,12 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Fridger.WindowsUniversalApp
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
+   
     sealed partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
+       
         public App()
-        {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
+        {            
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
@@ -50,54 +43,34 @@ namespace Fridger.WindowsUniversalApp
             }
 #endif
 
-            Frame rootFrame = Window.Current.Content as Frame;
+            AppShell shell = Window.Current.Content as AppShell;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
+            if (shell == null)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
+                shell = new AppShell();
 
-                rootFrame.NavigationFailed += OnNavigationFailed;
+                shell.AppFrame.NavigationFailed += OnNavigationFailed;
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     //TODO: Load state from previously suspended application
                 }
 
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
+                Window.Current.Content = shell;
             }
 
-            if (rootFrame.Content == null)
+            if (shell.AppFrame.Content == null)
             {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                shell.AppFrame.Navigate(typeof(HomePage), e.Arguments);
             }
-            // Ensure the current window is active
             Window.Current.Activate();
         }
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
         void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
---
+++ Fridger/Fridger.WindowsUniversalApp/AppShell.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Fridger.WindowsUniversalApp.Pages;
+using Fridger.WindowsUniversalApp.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Fridger.WindowsUniversalApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppShell : Page
+    {
+        public AppShell()
+        {
+            this.InitializeComponent();
+        }
+
+        public Frame AppFrame
+        {
+            get
+            {
+                return this.theFrame;
+            }
+        }
+
+        private void OnHomeAppBarButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.AppFrame.Navigate(typeof(Pages.HomePage));
+        }
+
+        private async void OnAddAppBarButtonClick(object sender, RoutedEventArgs e)
+        {
+            //this.AppFrame.Navigate(typeof(AddSuperheroPage));
+            var message = string.Format("You just clicked the button {0}", (sender as Button).Content);
+            var dialog = new MessageDialog(message);
+            dialog.Commands.Add(new UICommand("OK"));
+            await dialog.ShowAsync();
+        }
+
+        private void OnLoginAppBarButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.AppFrame.Navigate(typeof(LoginPage));
+        }
+    }
+}
